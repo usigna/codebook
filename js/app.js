116708100bfd1ccafc5e5a09392b650432995179
usigna <bukazaokna@gmail.com>
2020-11-17 22:51:11 +0100

    Add pixel btn and parallax effect

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 function changeTitle() {
-    const message = "Wróć na stronę";
+    const message = 'Wróć na stronę';
     let original;
 
-    window.addEventListener("focus", function() {
+    window.addEventListener('focus', function() {
         if (original) {
             document.title = original;
         }
     })
 
-    window.addEventListener("blur", function() {
+    window.addEventListener('blur', function() {
         const title = document.title;
         if (title != message) {
             original = title;
@@ -22,19 +22,37 @@ function getRandomColor() {
     return randomColor;
 }
 
-// another.html - circle-2
+// animation.html - circle-2
 function changeColorBorderCircles() {
-    const circles = document.querySelectorAll(".circle-2__span");
+    const circles = document.querySelectorAll('.circle-2__span');
 
     for (let i = 0; i < circles.length; i++) {
-        circles[i].style.borderColor = "#" + getRandomColor();
-        // circles[i].style.backgroundColor = "#" + getRandomColor();
+        circles[i].style.borderColor = '#' + getRandomColor();
+        // circles[i].style.backgroundColor = '#' + getRandomColor();
     }
+}
+
+// another.html - parallax
+function getParallaxEffect() {
+    const parallaxWrapper = document.querySelector('.parallax-1__wrapper');
+
+    function parallax(e) {
+        this.querySelectorAll('.parallax-1__layer').forEach(layer => {
+            const speed = layer.getAttribute('data-speed');
+            const x = (window.innerWidth - e.pageX * speed) / 100;
+            const y = (window.innerHeight - e.pageY * speed) / 100;
+
+            layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        })
+    }
+
+    parallaxWrapper.addEventListener('mousemove', parallax);
 }
 
 const init = function() {
     changeTitle();
     changeColorBorderCircles();
+    getParallaxEffect();
 };
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
